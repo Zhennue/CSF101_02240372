10fb6add44a14cc45a5fe059bbb3a27ee71f78d1
Zhennue <yesheyzhens2006@gmail.com>
2024-09-06 02:07:24 +0600

    Add files via upload

---
+++ csf101-python_exercises/csf101-python_exercises/Operators/Arthematic.py
@@ -0,0 +1,11 @@
+a, b = 15, 4
+print(f"a = {a}, b = {b}")
+
+print(f"Addition: {a+b}")
+print(f"Suntraction: {a-b}")
+print(f"Multiplication: {a*b}")
+print(f"Division: {a/b}")
+
+print(f"Modulus: {a%b}")
+print(f"Exponentiation: {a**b}")
+print(f"Floor Division: {a//b}")
